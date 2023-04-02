c6957f6f1baef8731b724159865f4c77f3df331a
abbosbek <abbosbekbotirjonovich@gmail.com>
2023-04-02 16:33:14 +0500

    add library

---
+++ app/src/main/java/abbosbek/mobiler/onlinesavdo/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package abbosbek.mobiler.onlinesavdo.activities
+
+import abbosbek.mobiler.onlinesavdo.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
---
+++ app/src/main/java/abbosbek/mobiler/onlinesavdo/fragments/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package abbosbek.mobiler.onlinesavdo.fragments
+
+import abbosbek.mobiler.onlinesavdo.R
+import androidx.fragment.app.Fragment
+
+class IntroductionFragment : Fragment(R.layout.fragment_introduction) {
+}
